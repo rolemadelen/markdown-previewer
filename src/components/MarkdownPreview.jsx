6ba28d7f2f92c209b47bb6ebb8b0e1d25e6cc058
rolemadelen <rolemadelen17@pm.me>
2022-09-18 20:18:25 +0900

    syntax highlighting added

--- src/components/MarkdownPreview.jsx
+++ src/components/MarkdownPreview.jsx
@@ -1,53 +1,119 @@
 import React from 'react';
-import DOMPurify from 'dompurify';
 import Header from './Header';
 import Footer from './Footer';
-import { marked } from 'marked';
-import "./MarkdownPreviewer.css";
+import './MarkdownPreviewer.css';
 import PageTopbar from './PageTopbar';
-import Prism from "prismjs";
-
-marked.setOptions({
-    breaks: true,
-    highlight: function () {
-        return Prism.highlightAll()
-    }
-});
+// import Prism from 'prismjs';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 export default class MarkdownPreview extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.handleContent = this.handleContent.bind(this);
-    }
+    this.state = {
+      markdown: '',
+    };
+    this.handleContent = this.handleContent.bind(this);
+  }
 
-    componentDidMount() {
-        let content = "# Hello \n\n [rolemadelen blog](https://rolemadelen.com) \n\n `code` \n\n ```cpp\ncodeblock\n``` \n\n - list1 \n - list2 \n\n > quote \n\n **bold text**";
-        document.querySelector("#editor").innerHTML = content;
-        document.querySelector("#preview").innerHTML = DOMPurify.sanitize(marked.parse(content));
-    }
+  componentDidMount() {
+    let content = `
+# Hello
 
-    handleContent(event) {
-        document.querySelector("#preview").innerHTML = DOMPurify.sanitize(marked.parse(event.target.value));
-    }
+[rolemadelen blog](https://rolemadelen.com)
 
-    render() {
-        return (
-            <React.StrictMode>
-                <div id="wrapper">
-                    <Header />
-                    <div id="editor-wrapper">
-                        <PageTopbar title={"editor"} />
-                        <textarea name="editor" id="editor" onChange={this.handleContent} />
-                    </div>
-                    <div id="preview-wrapper">
-                        <PageTopbar title={"preview"} />
-                        <div id="preview">
-                        </div>
-                    </div>
-                    <Footer />
-                </div>
-            </React.StrictMode>
-        )
-    }
+\`code\`
+
+\`\`\`cpp
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+\`\`\`
+
+- list1
+- list2
+
+**bold text**
+
+A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+
+A table:
+
+| a | b |
+| - | - |
+`;
+
+    document.querySelector('#editor').innerHTML = content;
+    this.setState({
+      markdown: content,
+    });
+  }
+
+  handleContent(event) {
+    this.setState({
+      markdown: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <React.StrictMode>
+        <div id="wrapper">
+          <Header />
+          <div id="editor-wrapper">
+            <PageTopbar title={'editor'} />
+            <textarea
+              name="editor"
+              id="editor"
+              onChange={this.handleContent}
+            />
+          </div>
+          <div id="preview-wrapper">
+            <PageTopbar title={'preview'} />
+            <ReactMarkdown
+              children={this.state.markdown}
+              remarkPlugins={[remarkGfm]}
+              components={{
+                code({
+                  node,
+                  inline,
+                  className,
+                  children,
+                  ...props
+                }) {
+                  const match = /language-(\w+)/.exec(
+                    className || ''
+                  );
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      children={String(children).replace(/\n$/, '')}
+                      style={dracula}
+                      language={match[1]}
+                      PreTag="div"
+                      {...props}
+                    />
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  );
+                },
+              }}
+            ></ReactMarkdown>
+          </div>
+          <Footer />
+        </div>
+      </React.StrictMode>
+    );
+  }
 }
