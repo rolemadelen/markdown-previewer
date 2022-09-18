4f45c250196f3c803bcd9f7fbadf2ac2818dc457
Madelen <rolemadelen@pm.me>
2022-09-18 20:18:25 +0900

    syntax highlighting added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,32 @@
-import React from "react";
-import "./Footer.css";
+import React from 'react';
+import './Footer.css';
 
 export default class Footer extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <React.StrictMode>
-                <footer>
-                    <p>by rolemadelen</p>
-                    <a href="https://github.com/rolemadelen/markdown-previewer" target={"_blank"}>
-                        <i className={"fa fa-github icon-github"} />
-                    </a>
-                    <a href="https://twitter.com/rolemadelen" target={"_blank"}>
-                        <i className={"fa fa-twitter icon-twitter"} />
-                    </a>
-                </footer>
-            </React.StrictMode>
-        )
-    }
+  render() {
+    return (
+      <React.StrictMode>
+        <footer>
+          <p>by rolemadelen</p>
+          <a
+            href="https://github.com/rolemadelen/markdown-previewer"
+            target={'_blank'}
+            rel="noreferrer"
+          >
+            <i className={'fa fa-github icon-github'} />
+          </a>
+          <a
+            href="https://twitter.com/rolemadelen"
+            target={'_blank'}
+            rel="noreferrer"
+          >
+            <i className={'fa fa-twitter icon-twitter'} />
+          </a>
+        </footer>
+      </React.StrictMode>
+    );
+  }
 }
