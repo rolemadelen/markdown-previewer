484de82ae9f258d0c7c7245f5d2cc39106fab539
Madelen <rolemadelen@pm.me>
2022-09-19 15:06:55 +0900

    separated into components

---
+++ src/components/Preview.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import remarkGfm from 'remark-gfm';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import PageTopbar from './PageTopbar';
+
+export default class Preview extends React.Component {
+  render() {
+    return (
+      <div id="preview-wrapper">
+        <PageTopbar title={'preview'} />
+        <ReactMarkdown
+          children={this.props.markdown}
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || '');
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  children={String(children).replace(/\n$/, '')}
+                  style={dracula}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                />
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        />
+      </div>
+    );
+  }
+}
